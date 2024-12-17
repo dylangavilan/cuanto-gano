6856a08a0615c4f0d3fbaef7718ebf3b80cf677a
Dylan Gavilan <73539254+dylangavilan@users.noreply.github.com>
2024-12-17 14:20:47 -0300

    Update

--- services/axios.ts
+++ services/axios.ts
@@ -7,12 +7,3 @@ const api = axios.create({
 })
 
 export default api
-
-const bcraApi = axios.create({
-    baseURL: 'https://api.estadisticasbcra.com',
-    headers:{
-        Authorization: 
-        `BEARER eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjQ1NDI4MzEsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJkeWxhbi5nYXZpbGFuMzJAZ21haWwuY29tIn0.h7dfMvdAMJoCdNb273YVOAsBCWVk8AVn-Z2ofZfTr-1w5Z1w82-P1C8Ti1WOsJHVePIE_5CRRXaVe4gyO8N20w`
-
-    }
-})
